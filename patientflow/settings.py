f0352ccf4dc045964f3b3b5724c348fa4f31605e
jonboland <jon.boland@gmail.com>
2022-01-28 16:19:40 +0000

    Apply LoginRequiredMixin

--- patientflow/settings.py
+++ patientflow/settings.py
@@ -143,3 +143,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'patients.User'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 LOGIN_REDIRECT_URL = '/patients'
+LOGIN_URL = "/login"
