8bb1ac3f11514d584b859544d0eb48e1fc4ceeaf
jonboland <jon.boland@gmail.com>
2022-02-03 17:39:12 +0000

    Add password reset urls and templates

--- patientflow/urls.py
+++ patientflow/urls.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
 from django.urls import path, include
 from patients.views import HomePageView, RegisterView
 
@@ -11,9 +18,25 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home-page'),
     path('patients/', include('patients.urls', namespace='patients')),
     path('staff/', include('staff.urls', namespace='staff')),
+    path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path(
+        'password-reset-done/',
+        PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'password-reset-confirm/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'password-reset-complete/',
+        PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
 
 if settings.DEBUG:
