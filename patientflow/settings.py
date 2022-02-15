9f61b9a18a7eca2ccb3ab73f92fea657df6f5625
jonboland <jon.boland@gmail.com>
2022-02-15 12:17:25 +0000

    Add postgres db settings

--- patientflow/settings.py
+++ patientflow/settings.py
@@ -93,9 +93,17 @@ WSGI_APPLICATION = 'patientflow.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
