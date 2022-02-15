7fe0712a6395099b543b7d1dc098826fd7373046
jonboland <jon.boland@gmail.com>
2022-02-15 12:15:01 +0000

    Add read .env setting

--- patientflow/settings.py
+++ patientflow/settings.py
@@ -22,9 +22,11 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(BASE_DIR / 'patientflow' / '.env')
+READ_DOT_ENV = env.bool('READ_DOT_ENV', default=False)
 
-# Quick-start development settings - unsuitable for production
+if READ_DOT_ENV == True:
+    environ.Env.read_env(BASE_DIR / 'patientflow' / '.env')
+
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
